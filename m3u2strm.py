6024779af9507d090cedff68fb6a5681e2c0fbc4
EagleMitchell <46403989+EagleMitchell@users.noreply.github.com>
2022-02-25 08:48:21 -0600

    Update m3u2strm.py
    Changed Line 21 from `src = '/m3u2strm/tvshows'` to `src = '/embyroot/m3u2strm/tvshows'` to reflect path in instructions.
    Changed Line 33 from `src = '/m3u2strm/movies'` to `src = '/embyroot/m3u2strm/movies'` to reflect path in instructions.

--- m3u2strm.py
+++ m3u2strm.py
@@ -18,7 +18,7 @@ if funct == 'alltv':
     print(wget.download(ipttvurl, ('m3u/apollotvshows.m3u')))
     apollolist = streamClasses.rawStreamList('m3u/apollotvshows.m3u')
     os.remove('m3u/apollotvshows.m3u')
-    src = '/m3u2strm/tvshows'
+    src = '/embyroot/m3u2strm/tvshows'
     dest = path
     print('copying folder structure to ',path)
     destination = shutil.copytree(src, dest, dirs_exist_ok=True)
@@ -30,7 +30,7 @@ elif funct == 'movies':
     print(wget.download(ipttvurl, ('m3u/iptvmovies.m3u')))
     apollolist = streamClasses.rawStreamList('m3u/iptvmovies.m3u')
     os.remove('m3u/iptvmovies.m3u')
-    src = '/m3u2strm/movies'
+    src = '/embyroot/m3u2strm/movies'
     dest = path
     print('copying folder structure to ',path)
     destination = shutil.copytree(src, dest, dirs_exist_ok=True)
